1c441abe3d18a5be00b88ba7ba27c02be5285cf1
Philippe De Hovre <phil@Philippes-Mac-mini.local>
2025-06-03 10:45:02 +0200

    add cloudinary to allowed remote pattern hosts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
 				protocol: "https",
 				hostname: "gravatar.com",
 			},
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com",
+			},
 		],
 	},
 };

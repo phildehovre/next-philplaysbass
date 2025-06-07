d95469eddb22a1bdc44287eba3f20e81c387fbf1
Philippe De Hovre <phil@Philippes-Mac-mini.local>
2025-06-07 15:07:38 +0200

    player is sort of working

--- components/metronome/SpotifyPlayer.tsx
+++ components/metronome/SpotifyPlayer.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+"use client";
+import React, { useState, useContext, useEffect } from "react";
+import { PlayerContext } from "@/context/playerContext";
+import "./SpotifyPlayer.css";
+import { Slider } from "../ui/slider";
+import PlayButton from "./PlayButton";
 
 const SpotifyPlayer = () => {
-	return <div>SpotifyPlayer</div>;
+	const [isOpen, setIsOpen] = useState(false);
+	const [position, setPosition] = useState<number>(0);
+	const { player, spotifyTrack, currentTrack, resumePosition } =
+		useContext<any>(PlayerContext);
+
+	useEffect(() => {
+		if (currentTrack) {
+			setIsOpen(true);
+		} else {
+			return;
+		}
+	}, [currentTrack]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			player?.getCurrentState().then((state: any) => {
+				if (state && state.duration > 0) {
+					const percent = (state.position / state.duration) * 100;
+					setPosition(percent);
+				}
+			});
+		}, 250);
+
+		return () => clearInterval(interval); // clean up
+	}, [player]);
+
+	return (
+		<div className={`player_ctn ${isOpen ? "open" : ""}`}>
+			<div className="header">
+				<h1>{currentTrack?.song_title}</h1>
+				<p>{currentTrack?.artist.name}</p>
+				<Slider value={[position]} max={100} step={1} />
+			</div>
+		</div>
+	);
 };
 
 export default SpotifyPlayer;

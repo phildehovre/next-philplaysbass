99fdabfba7b8a4a548661482373349978a4a5f2e
Philippe De Hovre <phil@Philippes-Mac-mini.local>
2025-06-09 10:30:12 +0200

    types fix

--- types/types.tsx
+++ types/types.tsx
@@ -71,13 +71,6 @@ export type SongData = {
 declare global {
 	interface Window {
 		onSpotifyWebPlaybackSDKReady: () => void;
-		Spotify: {
-			Player: new (options: {
-				name: string;
-				getOAuthToken: (cb: (token: string) => void) => void;
-				volume?: number;
-			}) => SpotifyPlayer;
-		};
 	}
 }
 

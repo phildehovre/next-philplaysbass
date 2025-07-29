ed3915f81f2458c883cbbb3959aa3efca071bc42
Philippe De Hovre <ph.dehovre@gmail.com>
2025-07-29 11:50:51 +0200

    remove try/catch from [kindeauth]/route.js

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,13 +1,2 @@
-// import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-// export const GET = handleAuth();
-
-import { handleRedirectToApp } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export async function GET(req) {
-	try {
-		return await handleRedirectToApp(req);
-	} catch (error) {
-		console.error("🔴 Kinde callback error:", error);
-		return new Response("Kinde callback failed", { status: 500 });
-	}
-}
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();

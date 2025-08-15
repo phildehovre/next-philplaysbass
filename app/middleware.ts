42fd9c9d38138e575beb24e7a59085f2b9c4787d
Philippe De Hovre <ph.dehovre@gmail.com>
2025-08-15 16:34:44 +0200

    excluse lwm from auth flow in middleware.ts

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,9 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
 	const { pathname } = request.nextUrl;
-	// const token = request.cookies.get("auth_token");
 
 	const { isAuthenticated } = getKindeServerSession();
+
+	// Skip auth check for /learnwithmarco
+	if (pathname.startsWith("/learnwithmarco")) {
+		return NextResponse.next();
+	}
 
 	if (pathname.startsWith("/api") && !(await isAuthenticated())) {
 		return NextResponse.redirect(new URL("/login", request.url));
